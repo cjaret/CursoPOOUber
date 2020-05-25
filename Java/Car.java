e3a1c6508dd02d2531da32ad6ebe68c52a31705c
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-05-25 16:05:22 -0500

    Clase 22: Creamos el método constructor en Car.java

--- Java/Car.java
+++ Java/Car.java
@@ -1,10 +1,22 @@
 class Car {
-    Integer id;
+    Integer id; /* Variables globales respecto a la clase */
     String license;
-    String driver;
+    Account driver; /* Cambiamos el tipo de dato para driver de String a tipo Account porque en nuestro diagrama de herencia driver hereda de account */
     Integer passenger; 
 
-    /* Vamos a declarar un método que me sirva para estar trayendo los datos de esta clase: */
+    /* Método constructor: */
+
+    public Car(/* Aquí dentro colocamos los parámetros que quiero que sean obligatorios para que se cree un objeto de tipo Car */ String license, Account driver/* A estos parámetros se les acostumbra poner (buena práctica), el mismo nombre  de los atributos arriba declarados. Es buena práctica porque de esta forma es más consistente la información y es más intuitivo programar.*/ /* Estos parámetros están siendo declarados de manera local al método Car*/){
+
+        /* Si quiero que cuando alguien le pase un dato a license automáticamente se inicialice el atributo license de la Clase, lo que tenemos que hacer es llamar a ese atributo */
+
+        this.license = license;
+        this.driver = driver;
+
+        /* Al colocar this vamos a tener acceso a todos los atributos de esta clase (Car.java), incluso a los métodos como el que se encuentra abajo de este método constructor */
+        
+
+    }
 
     void printDataCar(){
         System.out.println("Licence: " + license);
