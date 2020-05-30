395393e6ad0ab7d24343fb524872aece85ddb8ce
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-05-30 12:45:24 -0500

    Clase 27. Aplicando herencia en JS

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,7 @@
-var car = new Car("QWT465", new Account("Carlitos", "CURP"))
+var car = new Car("QWT465", new Account("CDJR", "CURP"))
 car.passenger = 4;
-
 car.printDataCar();
+
+var uberX = new UberX("RYE257", new Account("The great Daniel", "CURP"), "Tesla", "X")
+uberX.passenger = 4;
+uberX.printDataCar();
