a33c6970a612fe42f3a20cf468e4df138f9ab899
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-05-30 12:14:50 -0500

    Clase 26. Solución del reto de herencia en PHP / Clase 27. Aplicando herencia en Python.

--- PHP/index.php
+++ PHP/index.php
@@ -4,8 +4,9 @@ require_once("uberX.php");
 require_once("uberPool.php");
 require_once("account.php");
 
+/* Objeto tipo Uber X: -> */
 $uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
-$uberX->printDataCar();
+$uberX->printDataCar(); /* Método heredado de la clase UberX, de la clase Car. */
 
 $uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
 $uberPool->printDataCar();
